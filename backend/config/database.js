70c6cb6e81e912167fbc0fef435e13e838bfd5d3
shediv <videshhedge@gmail.com>
2019-04-19 17:41:44 +0530

    New material design

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,13 +2,13 @@ const mongoose = require('mongoose');
 const server = '127.0.0.1:27017'; // REPLACE WITH YOUR DB SERVER
 const database = 'mean-material-app-db';      // REPLACE WITH YOUR DB NAME
 const localConnectionString = `mongodb://${server}/${database}`;
-const remoteConnectionString = `mongodb+srv://new-user_31:${process.env.passphrase}@cluster0-pke4z.mongodb.net/test?retryWrites=true/mean-material-app-db`;
+const remoteConnectionString = `mongodb://developer:!recordtv8@ds145146.mlab.com:45146/text-audio`;
 class Database {
     constructor() {
         this._connect();
     }
     _connect() {
-        mongoose.connect(localConnectionString)
+        mongoose.connect(remoteConnectionString)
             .then(() => {
                 console.log('Database connection successful')
             })
